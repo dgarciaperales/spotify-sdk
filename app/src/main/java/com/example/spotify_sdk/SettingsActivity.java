c0829cb0c878bebdb620a19a0cd8307b9a33d25a
dgarciaperales <dgarciaperales04@gmail.com>
2024-03-21 15:52:50 -0400

    Update SettingsActivity.java

--- app/src/main/java/com/example/spotify_sdk/SettingsActivity.java
+++ app/src/main/java/com/example/spotify_sdk/SettingsActivity.java
@@ -3,6 +3,11 @@ package com.example.spotify_sdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //navigation to home/wrapped page
+        ImageButton wrappedButton = findViewById(R.id.wrapped_btn);
+        wrappedButton.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        //navigation to feed page
+        ImageButton feedButton = findViewById(R.id.feed_btn);
+        feedButton.setOnClickListener( v -> {
+            Intent intent = new Intent(SettingsActivity.this, FeedActivity.class);
+            startActivity(intent);
+        });
     }
 }
