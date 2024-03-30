19b714729b87115ce2cfcb1370b5cde722e2997b
dgarciaperales <dgarciaperales04@gmail.com>
2024-03-30 10:43:33 -0400

    Update GenerateWrapped.java

--- app/src/main/java/com/example/spotify_sdk/GenerateWrapped.java
+++ app/src/main/java/com/example/spotify_sdk/GenerateWrapped.java
@@ -9,6 +9,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,6 +30,8 @@ public class GenerateWrapped extends AppCompatActivity {
     private String mAccessToken;
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private Call mCall;
+
+    private String firstArtistId;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -108,6 +111,7 @@ public class GenerateWrapped extends AppCompatActivity {
                                 try {
                                     final JSONObject artistsObject = new JSONObject(response.body().string());
                                     final JSONArray artistsArray = artistsObject.getJSONArray("items");
+                                    firstArtistId = artistsArray.getJSONObject(0).getString("id");
                                     ArrayList<String> artists = new ArrayList<>();
                                     for (int i = 0; i < artistsArray.length(); i++) {
                                         artists.add(artistsArray.getJSONObject(i).getString("name"));
@@ -160,12 +164,58 @@ public class GenerateWrapped extends AppCompatActivity {
                                                                     }
                                                                 }
                                                             }
-                                                            Bundle bundle = new Bundle();
-                                                            bundle.putStringArrayList("tracks", tracks);
-                                                            bundle.putStringArrayList("artists", artists);
-                                                            bundle.putStringArrayList("genres", genres);
 
-                                                            setTextAsync("\nTop Tracks: " + tracks.toString() + "\n\nTop Artists: " + artists.toString() + "\n\nTop Genres: " + genres.toString(), profileTextView);
+                                                            final Request relatedArtistsRequest = new Request.Builder()
+                                                                    .url("https://api.spotify.com/v1/artists/" + firstArtistId + "/related-artists")
+                                                                    .addHeader("Authorization", "Bearer " + mAccessToken)
+                                                                    .build();
+
+                                                            mOkHttpClient.newCall(relatedArtistsRequest).enqueue(new Callback() {
+                                                                @Override
+                                                                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                                                                    Log.d("JSON", "Failed process recs API: " + e);
+                                                                    showToast("Failed to process recs API, watch Logcat for more details");
+                                                                }
+
+                                                                @Override
+                                                                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                                                                    try {
+                                                                        if (!response.isSuccessful()) {
+                                                                            Log.e("Response", "Unsucessful response code: " + response.code());
+                                                                            showToast("Unsucessful response code: " + response.code());
+                                                                            return;
+                                                                        }
+                                                                        String responseBody = response.body().string();
+                                                                        Log.d("Response", "Response body: " + responseBody);
+
+                                                                        JSONObject relatedArtistsObject = new JSONObject(responseBody);
+                                                                        JSONArray relatedArtistsArray = relatedArtistsObject.getJSONArray("artists");
+                                                                        ArrayList<String> relatedArtists = new ArrayList<>();
+                                                                        int count = Math.min(relatedArtistsArray.length(), 5);
+                                                                        for (int i = 0; i < count; i++) {
+                                                                            JSONObject artistObject = relatedArtistsArray.getJSONObject(i);
+                                                                            String artistName = artistObject.getString("name");
+                                                                            relatedArtists.add(artistName);
+                                                                        }
+
+                                                                        Bundle bundle = new Bundle();
+                                                                        bundle.putStringArrayList("tracks", tracks);
+                                                                        bundle.putStringArrayList("artists", artists);
+                                                                        bundle.putStringArrayList("genres", genres);
+                                                                        bundle.putStringArrayList("Recommended Artists", relatedArtists);
+
+                                                                        setTextAsync("\nTop Tracks: " + tracks.toString() + "\n\nTop Artists: " + artists.toString() + "\n\nTop Genres: " + genres.toString() + "\n\n Recommended Artists: " + relatedArtists.toString(), profileTextView);
+
+                                                                    }
+                                                                    catch (JSONException e) {
+                                                                        Log.d("JSON", "Failed to parse recommendations: " + e);
+                                                                        showToast("Failed to parse recommendations, watch Logcat for more details");
+                                                                    }
+                                                                }
+                                                            });
+
+
+
                                                         } catch (JSONException e) {
                                                             Log.d("JSON", "Failed to parse genres: " + e);
                                                             showToast("Failed to parse genres, watch Logcat for more details");
