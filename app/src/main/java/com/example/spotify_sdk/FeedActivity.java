c924b2c4443a972db4e4d6806b39ca7a46527b33
dgarciaperales <dgarciaperales04@gmail.com>
2024-04-02 23:42:30 -0400

    Update FeedActivity.java

--- app/src/main/java/com/example/spotify_sdk/FeedActivity.java
+++ app/src/main/java/com/example/spotify_sdk/FeedActivity.java
@@ -31,14 +31,14 @@ public class FeedActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        ImageButton addFriendsBtn=  (ImageButton) findViewById(R.id.addFriendsButton);
-
-        // Below code is for setting a click listener on the image
-        addFriendsBtn.setOnClickListener(view -> {
-            // Creating a toast to display the message
-            Toast.makeText(FeedActivity.this, "Add Friends Page", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(FeedActivity.this, AddFriendsActivity.class);
-            startActivity(intent);
-        });
+//        ImageButton addFriendsBtn=  (ImageButton) findViewById(R.id.addFriendsButton);
+//
+//        // Below code is for setting a click listener on the image
+//        addFriendsBtn.setOnClickListener(view -> {
+//            // Creating a toast to display the message
+//            Toast.makeText(FeedActivity.this, "Add Friends Page", Toast.LENGTH_SHORT).show();
+//            Intent intent = new Intent(FeedActivity.this, AddFriendsActivity.class);
+//            startActivity(intent);
+//        });
     }
 }
