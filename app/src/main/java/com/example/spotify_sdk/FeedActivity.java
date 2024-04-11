fe41094a5d3fbe517b7b0a3ab84e030e2f3f009a
akshi <neni.akki@gmail.com>
2024-04-11 00:39:05 -0400

    Connected feed to firebase, now displays correct info

--- app/src/main/java/com/example/spotify_sdk/FeedActivity.java
+++ app/src/main/java/com/example/spotify_sdk/FeedActivity.java
@@ -14,12 +14,27 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
 public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
         LinearLayout scrollContentLayout = findViewById(R.id.postLayout);
 
@@ -38,32 +53,21 @@ public class FeedActivity extends AppCompatActivity {
         });
 
         Button refreshBtn = findViewById(R.id.refresh_btn);
+        final Integer[] postCounter = {0};
+        DocumentReference postStats = firestore.collection("posts").document("postStats");
+
         refreshBtn.setOnClickListener(v -> {
-
-            for (int i = 1; i < 4; i++) {
-                View cardView = LayoutInflater.from(this).inflate(R.layout.post_card, null);
-                TextView topArtist = cardView.findViewById(R.id.topArtist);
-                TextView topGenre = cardView.findViewById(R.id.topGenre);
-                TextView topTrack1 = cardView.findViewById(R.id.topTrack1);
-                TextView topTrackArtist1 = cardView.findViewById(R.id.topTrackArtist1);
-                TextView topTrack2 = cardView.findViewById(R.id.topTrack2);
-                TextView topTrackArtist2 = cardView.findViewById(R.id.topTrackArtist2);
-                TextView topTrack3 = cardView.findViewById(R.id.topTrack3);
-                TextView topTrackArtist3 = cardView.findViewById(R.id.topTrackArtist3);
-
-                topArtist.setText("my top artist : " + i);
-                topGenre.setText("my top genre : " + i);
-
-                for (int f = 1; f < 4; f++) {
-                    topTrack1.setText("my top track : " + f);
-                    topTrackArtist1.setText("my top track artist: " + f);
-                    topTrack2.setText("my top track : " + f);
-                    topTrackArtist2.setText("my top track artist: " + f);
-                    topTrack3.setText("my top track : " + f);
-                    topTrackArtist3.setText("my top track artist: " + f);
+            //getting number of posts
+            postStats.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if(documentSnapshot != null){
+                        Long postCounterLong = documentSnapshot.getLong("postCounter");
+                        postCounter[0] = (Integer) postCounterLong.intValue();
+                        updateFeed(postCounter[0], scrollContentLayout);
+                    }
                 }
-                scrollContentLayout.addView(cardView);
-            }
+            });
         });
 
 //        ImageButton commentButton =  (ImageButton) findViewById(R.id.comment_button);
@@ -72,5 +76,45 @@ public class FeedActivity extends AppCompatActivity {
 //            Intent intent = new Intent(FeedActivity.this, CommentViewActivity.class);
 //            startActivity(intent);
 //        });
+    }
+
+    private void updateFeed(Integer totalPosts, LinearLayout scrollContentLayout){
+        for (int i = 0; i <= totalPosts - 1; i++) {
+            View cardView = LayoutInflater.from(this).inflate(R.layout.post_card, null);
+            TextView topArtist = cardView.findViewById(R.id.topArtist);
+            TextView topGenre = cardView.findViewById(R.id.topGenre);
+            TextView topTrack1 = cardView.findViewById(R.id.topTrack1);
+            TextView topTrackArtist1 = cardView.findViewById(R.id.topTrackArtist1);
+            TextView topTrack2 = cardView.findViewById(R.id.topTrack2);
+            TextView topTrackArtist2 = cardView.findViewById(R.id.topTrackArtist2);
+            TextView topTrack3 = cardView.findViewById(R.id.topTrack3);
+            TextView topTrackArtist3 = cardView.findViewById(R.id.topTrackArtist3);
+            TextView username = cardView.findViewById(R.id.username);
+
+            CollectionReference posts = firestore.collection("posts");
+            posts.document("post"+i).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if(documentSnapshot != null){
+                        List<String> topTracks = (List<String>) documentSnapshot.get("topTracks");
+                        //List<String> topTrackArtists = (List<String>) documentSnapshot.get("topTrackArtists");
+                        String topArtistString = documentSnapshot.getString("topArtist");
+                        String topGenreString = documentSnapshot.getString("topGenre");
+                        String usernameString = documentSnapshot.getString("author");
+                        username.setText(usernameString);
+                        topArtist.setText("my top artist : " + topArtistString);
+                        topGenre.setText("my top genre : " + topGenreString);
+                        topTrack1.setText("my top track : " + topTracks.get(0));
+                        //topTrackArtist1.setText("my top track artist: " + topTrackArtists.get(f));
+                        topTrack2.setText("my top track : " + topTracks.get(1));
+                        //topTrackArtist2.setText("my top track artist: " + topTrackArtists.get(f));
+                        topTrack3.setText("my top track : " + topTracks.get(2));
+                        //topTrackArtist3.setText("my top track artist: " + topTrackArtists.get(f));
+                        scrollContentLayout.addView(cardView);
+                    }
+
+                }
+            });
+        }
     }
 }
